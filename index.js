1cb502b5ef064610c6b1b9d2a8676ba3ffb2deb5
imran-mridha <mdimranmridha.pro@gmail.com>
2022-11-08 02:16:19 +0600

    Added Mongodb

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.dmnves2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    
+  }
+  finally{
+
+  }
+}
+
+run().catch(err => console.error(err))
+
 
 app.get('/',(req,res)=>{
   res.send('Server Running')
